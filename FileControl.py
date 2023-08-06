3b747fb99170383fa4724b473508f8367a769c36
zzaizzai <junsai0530@gmail.com>
2023-08-06 22:58:09 +0900

    extension

--- FileControl.py
+++ FileControl.py
@@ -11,12 +11,31 @@ def is_file(path: Path) -> bool:
 def is_dir(path: Path) -> bool:
     return os.path.isdir(path)
 
+def is_xlsx(path: Path) -> bool:
+    extension = os.path.splitext(os.path.basename(path))[-1][1:]
+
+    if extension == 'xlsx' or extension == 'xls':
+        return True
+    
+    return False
+
+def is_extension(path: Path, extension_str: str) -> bool:
+    extension = os.path.splitext(os.path.basename(path))[-1][1:]
+
+    if extension == extension_str:
+        return True
+    
+    return False
 
 def get_current_path() -> Path:
     return Path(os.getcwd())
 
+def make_dir(path: Path, dir_name: str) -> None:
+    new_dir_path = str(path) + '\\' + dir_name
+    os.makedirs(new_dir_path, exist_ok=True)
 
-def get_file_name_with_extension(file_path: Path) -> str: 
+
+def get_file_name_with_extension(file_path: Path) -> str | None: 
     """
     C:\\Users\\user\\Desktop\\dir\\file.txt
 
@@ -24,14 +43,14 @@ def get_file_name_with_extension(file_path: Path) -> str:
     """
     return os.path.basename(file_path) 
 
-def get_file_name_without_extension(file_path: Path) -> str:
+def get_file_name_without_extension(file_path: Path) -> str | None:
     """
     C:\\Users\\user\\Desktop\\dir\\text.txt
     
     =>file"""
     return os.path.splitext(os.path.basename(file_path))[0] 
 
-def get_file_name_extension(file_path: Path) -> str:
+def get_file_name_extension(file_path: Path) -> str | None:
     """
     C:\\Users\\user\\Desktop\\dir\\text.txt
 
@@ -39,7 +58,7 @@ def get_file_name_extension(file_path: Path) -> str:
     """
     return os.path.splitext(os.path.basename(file_path))[-1][1:]
 
-def get_pardir_path(file_path: Path) -> Path:
+def get_pardir_path(file_path: Path) -> Path | None:
     """
     C:\\Users\\user\\Desktop\\dir\\text.txt
 
@@ -52,7 +71,7 @@ def get_pardir_path(file_path: Path) -> Path:
     
     return 
 
-def get_pardir_name(file_path: Path) -> str:
+def get_pardir_name(file_path: Path) -> str | None:
     """
     C:\\Users\\user\\Desktop\\dir\\text.txt
 
