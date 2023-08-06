44aba6fe7a644178b0e8637192e1e3f79acba08e
zzaizzai <junsai0530@gmail.com>
2023-08-06 17:20:10 +0900

    gitignore

---
+++ test.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+import FileControl as fc
+
+
+if __name__ == '__main__':
+    file_path = str(fc.get_current_path()) + r'\test.txt'
+    print(fc.get_file_name_extension(file_path))
+    print(fc.get_pardir_name(Path(file_path)))
+    print(fc.is_file(file_path))
