aab03ff68481867731af87ca1989faef8163f2a9
zzaizzai <junsai0530@gmail.com>
2023-08-06 17:16:46 +0900

    simpe def

--- FileControl.py
+++ FileControl.py
@@ -2,6 +2,19 @@ import os
 from pathlib import Path
 
 
+def check_exist(path: Path) -> bool:
+    return os.path.exists(path)
+
+def is_file(path: Path) -> bool:
+    return os.path.isfile(path)
+
+def is_dir(path: Path) -> bool:
+    return os.path.isdir(path)
+
+
+def get_current_path() -> Path:
+    return Path(os.getcwd())
+
 
 def get_file_name_with_extension(file_path: Path) -> str: 
     """
@@ -25,7 +38,6 @@ def get_file_name_extension(file_path: Path) -> str:
     =>txt
     """
     return os.path.splitext(os.path.basename(file_path))[-1][1:]
-
 
 def get_pardir_path(file_path: Path) -> Path:
     """
@@ -54,10 +66,9 @@ def get_pardir_name(file_path: Path) -> str:
     
     return
 
-def get_current_path() -> Path:
-    return Path(os.getcwd())
 
 if __name__ == '__main__':
-    filename = str(get_current_path()) + r'\test.txt'
-    print(get_file_name_extension(filename))
-    print(get_pardir_name(Path(filename)))
+    file_path = str(get_current_path()) + r'\test.txt'
+    print(get_file_name_extension(file_path))
+    print(get_pardir_name(Path(file_path)))
+    print(is_file(file_path))
